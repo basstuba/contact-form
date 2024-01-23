2a654ed54346a881685128b3e7ba68f3fd7cc170
nutka <tuba4080@outlook.com>
2024-01-23 10:03:57 +0100

    uodate

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -18,10 +18,12 @@
             </div>
             <div class="form__group-content">
                 <div class="form__input--text">
-                    <input type="text" name="name" placeholder="テスト太郎"/>
+                    <input type="text" name="name" placeholder="テスト太郎" value="{{old('name')}}"/>
                 </div>
                 <div class="form__error">
-                    <!--バリエーション機能実装後に記述-->
+                    @error('name')
+                    {{$message}}
+                    @enderror
                 </div>
             </div>
         </div>
@@ -32,10 +34,12 @@
             </div>
             <div class="form__group-content">
                 <div class="form__input--text">
-                    <input type="email" name="email" placeholder="test@example.com"/>
+                    <input type="email" name="email" placeholder="test@example.com" value="{{old('email')}}"/>
                 </div>
                 <div class="form__error">
-                    <!--バリエーション機能実装後に記述-->
+                    @error('email')
+                    {{$message}}
+                    @enderror
                 </div>
             </div>
         </div>
@@ -46,10 +50,12 @@
             </div>
             <div class="form__group-content">
                 <div class="form__input--text">
-                    <input type="tel" name="tel" placeholder="09012345678"/>
+                    <input type="tel" name="tel" placeholder="09012345678" value="{{old('tel')}}"/>
                 </div>
                 <div class="form__error">
-                    <!--バリエーション機能実装後に記述-->
+                    @error('tel')
+                    {{$message}}
+                    @enderror
                 </div>
             </div>
         </div>
